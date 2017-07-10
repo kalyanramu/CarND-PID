cb9d29bb0b2db958bfc790d84dff5e00bc9ecdc5
kalyanramu <kalyanramu@gmail.com>
2017-07-10 00:14:00 -0500

    Car steering properly at 30mph

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,6 @@
 #include "PID.h"
+#include <iostream>
+#include <chrono> //http://www.compspace.org/how-to-measure-elapsed-time-in-cpp-code/
 
 using namespace std;
 
@@ -8,14 +10,38 @@ using namespace std;
 
 PID::PID() {}
 
-PID::~PID() {}
+PID::~PID() {
+
+}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = 0.1;
+    this->Ki = 0.1;
+    this->Kd = 0.1;
+
+    p_error = 0;
+    d_error = 0;
+    i_error = 0;
+
+    prev_time = std::chrono::system_clock::now();
 }
 
 void PID::UpdateError(double cte) {
+    //cout << "Cross Track Error: " << cte << endl;
+    current_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = (current_time - prev_time);//current_time - prev_time;
+    dt = elapsed_seconds.count();
+    cout << "duration: " << dt << endl;
+
+    p_error = cte;
+    d_error = (cte - prev_cte)/dt;
+    i_error += cte*dt;
+    prev_cte = cte;
+
+    prev_time = current_time;
 }
 
 double PID::TotalError() {
+return p_error*Kp + d_error*Kd + i_error*Ki;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -1,6 +1,8 @@
 #ifndef PID_H
 #define PID_H
 
+#include <chrono>
+
 class PID {
 public:
   /*
@@ -16,6 +18,12 @@ public:
   double Kp;
   double Ki;
   double Kd;
+
+  //Interim Values
+  double prev_cte;
+  std::chrono::time_point<std::chrono::system_clock> prev_time;
+  std::chrono::time_point<std::chrono::system_clock> current_time;
+  double dt;
 
   /*
   * Constructor
